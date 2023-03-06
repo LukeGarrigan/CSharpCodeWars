d83c54c1db18dfdba89b5e95b118f2d8c6454ed2
Luke <lukegarrigan8@gmail.com>
2023-03-06 08:38:19 +0000

    further improvements to alpinist

--- CSharpCodeWars/Kyu3/PathFinder3TheAlpinist/PathFinder3TheAlpinist.cs
+++ CSharpCodeWars/Kyu3/PathFinder3TheAlpinist/PathFinder3TheAlpinist.cs
@@ -1,126 +1,91 @@
 using System;
 
-namespace CSharpCodeWars.Kyu3.PathFinder3TheAlpinist
+namespace CSharpCodeWars.Kyu3.PathFinder3TheAlpinist;
+
+public class PathFinder3TheAlpinist
 {
-    public class PathFinder3TheAlpinist
+    public int PathFinder(string mazeAsString)
     {
-        public int PathFinder(string mazeAsString)
-        {
-            var maze = ConvertToArray(mazeAsString);
-            var open = new PriorityQueue<Position, int>();
-            var closed = new HashSet<Position>();
+        var maze = ConvertToArray(mazeAsString);
+        var open = new PriorityQueue<Position, int>();
+        var closed = new HashSet<Position>();
 
-            var size = maze.GetLength(0) - 1; 
+        var size = maze.GetLength(0) - 1; 
             
-            var goalPos = new Position(size, size, -1);
-            var startPos = new Position(0, 0, 0);
-            closed.Add(startPos);
-            open.Enqueue(startPos, 0);
+        var goalPos = new Position(size, size);
+        var startPos = new Position(0, 0);
+        closed.Add(startPos);
+        open.Enqueue(startPos, 0);
 
-            while (open.Count > 0)
-            {
-                var current = open.Dequeue();
-                closed.Add(current);
-                
-                if (current.Equals(goalPos))
-                {
-                    return current.Count;
-                }
-                
-                foreach (var child in GetNeighbours(current, maze, size).Where(pos => !closed.Contains(pos)))
-                {
-                    open.Enqueue(child, child.Count);   
-                }
-            }
-
-            return -1;
-        }
-
-        private static IEnumerable<Position> GetNeighbours(Position current, int[,] maze, int size)
+        while (open.TryDequeue(out var current, out int priority))
         {
-            var possibleMoves = new List<Position>();
-            // N
-            if (current.Y > 0)
+            closed.Add(current);
+                
+            if (current.Equals(goalPos))
             {
-                var difference = Math.Abs(maze[current.X, current.Y] - maze[current.X, current.Y - 1]);
-                possibleMoves.Add(new Position(current.X, current.Y - 1, current.Count + difference));
+                return priority;
             }
-
-            // S
-            if (current.Y < size)
+                
+            foreach (var child in GetNeighbours(current, maze, size).Where(pos => !closed.Contains(pos)))
             {
-                var difference = Math.Abs(maze[current.X, current.Y] - maze[current.X, current.Y + 1]);
-                possibleMoves.Add(new Position(current.X, current.Y + 1, current.Count + difference));
+                    
+                var difference = Math.Abs(maze[current.X, current.Y] - maze[child.X, child.Y]);
+                open.Enqueue(child, priority + difference);   
             }
-
-            // W
-            if (current.X > 0)
-            {
-                var difference = Math.Abs(maze[current.X, current.Y] - maze[current.X - 1, current.Y]);
-                possibleMoves.Add(new Position(current.X - 1, current.Y, current.Count + difference));
-            }
-
-            // E
-            if (current.X < size)
-            {
-                var difference = Math.Abs(maze[current.X, current.Y] - maze[current.X + 1, current.Y]);
-                possibleMoves.Add(new Position(current.X + 1, current.Y, current.Count + difference));
-            }
-
-            return possibleMoves;
         }
 
-        private static int[,] ConvertToArray(string mazeAsString)
-        {
-            var lines = mazeAsString.Split("\n");
-            var xLength = lines[0].Length;
-            var yLength = lines.Length;
-
-
-            var maze = new int[xLength, yLength];
-
-            for (var y = 0; y < yLength; y++)
-            {
-                for (var x = 0; x < xLength; x++)
-                {
-                    int.TryParse(lines[y][x].ToString(), out maze[y, x]);
-                }
-            }
-
-            return maze;
-        }
+        return -1;
     }
 
-    class Position 
+    private static IEnumerable<Position> GetNeighbours(Position current, int[,] maze, int size)
     {
-
-        public int X { get; set; }
-        public int Y { get; set; }
-        public int Count { get; set; }
-        public int F { get; set; }
-        
-        public Position(int x, int y, int count)
+        var possibleMoves = new List<Position>();
+        // N
+        if (current.Y > 0)
         {
-            X = x;
-            Y = y;
-            Count = count;
+            possibleMoves.Add(new Position(current.X, current.Y - 1));
         }
-        
 
-        public override bool Equals(object obj)
+        // S
+        if (current.Y < size)
         {
-            if (obj == null || !(obj is Position))
+            possibleMoves.Add(new Position(current.X, current.Y + 1));
+        }
+
+        // W
+        if (current.X > 0)
+        {
+            possibleMoves.Add(new Position(current.X - 1, current.Y));
+        }
+
+        // E
+        if (current.X < size)
+        {
+            possibleMoves.Add(new Position(current.X + 1, current.Y));
+        }
+
+        return possibleMoves;
+    }
+
+    private static int[,] ConvertToArray(string mazeAsString)
+    {
+        var lines = mazeAsString.Split("\n");
+        var xLength = lines[0].Length;
+        var yLength = lines.Length;
+
+
+        var maze = new int[xLength, yLength];
+
+        for (var y = 0; y < yLength; y++)
+        {
+            for (var x = 0; x < xLength; x++)
             {
-                return false;
+                int.TryParse(lines[y][x].ToString(), out maze[y, x]);
             }
-
-            var other = (Position)obj;
-            return X == other.X && Y == other.Y;
         }
 
-        public override int GetHashCode()
-        {
-            return X.GetHashCode() ^ Y.GetHashCode();
-        }
+        return maze;
     }
 }
+
+record Position(int X, int Y);

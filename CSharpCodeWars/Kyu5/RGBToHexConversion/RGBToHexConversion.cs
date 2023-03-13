5a7a27be5dd9d937eb499830e9dd517dac12daf4
Luke <lukegarrigan8@gmail.com>
2023-03-13 08:49:40 +0000

    little refactor based on some solutions

--- CSharpCodeWars/Kyu5/RGBToHexConversion/RGBToHexConversion.cs
+++ CSharpCodeWars/Kyu5/RGBToHexConversion/RGBToHexConversion.cs
@@ -6,30 +6,9 @@ public class RGBToHexConversion
 {
     public string RgbToHex(int r,int g, int b)
     {
-        if (r < 0) r = 0;
-        if (r > 255) r = 255;
-        if (g < 0) g = 0;
-        if (g > 255) g = 255;
-        if (b < 0) b = 0;
-        if (b > 255) b = 255;
-
-        var rHex = r.ToString("x"); // could have used x2 here and I wouldn't have had to pad
-        var gHex = g.ToString("x");
-        var bHex = b.ToString("x");
-
-        rHex = Pad(rHex); 
-        gHex = Pad(gHex);
-        bHex = Pad(bHex);
+        var rHex = Math.Max(Math.Min(r, 255), 0).ToString("x2");
+        var gHex = Math.Max(Math.Min(g, 255), 0).ToString("x2");
+        var bHex = Math.Max(Math.Min(b, 255), 0).ToString("x2");
         return $"{rHex}{gHex}{bHex}".ToUpper();
-    }
-
-    private static string Pad(string hex)
-    {
-        if (hex.Length == 1)
-        {
-            hex = $"0{hex}";
-        }
-
-        return hex;
     }
 }

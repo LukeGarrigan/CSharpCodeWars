957bfae9d98ece13c44ca5a805f2ac0c4f9f55e4
Luke <lukegarrigan8@gmail.com>
2023-03-21 08:59:28 +0000

    TakeWhile, oh how lovely

--- CSharpCodeWars/Kyu6/LongestConsecutiveRepetition/LongestConsecutiveRepetition.cs
+++ CSharpCodeWars/Kyu6/LongestConsecutiveRepetition/LongestConsecutiveRepetition.cs
@@ -7,32 +7,36 @@ public class LongestConsecutiveRepetition
 {
     public Tuple<char?, int> LongestRepetition(string input)
     {
-        var current = '0';
-        var currentCount = 0;
-        var biggest = 0;
-        var biggestChar = '0';
-        for (int i = 0; i < input.Length; i++)
-        {
-            var currentChar = input[i];
+        return input
+                   .Select((x, i) => new Tuple<Char?, Int32>(x, input.Substring(i).TakeWhile(y => y == x).Count()))
+                   .MaxBy(x => x.Item2)
+               ?? new Tuple<char?, Int32>(null, 0);
 
-            if (currentChar == current)
-            {
-                currentCount++;
-            }
-            else
-            {
-                current = currentChar;
-                currentCount = 1;
-            }
-
-            if (currentCount > biggest)
-            {
-                biggest = currentCount;
-                biggestChar = currentChar;
-            }
-        }
-
-        return new Tuple<char?, int>(biggestChar == '0' ? null : biggestChar, biggest);
-
+        // var current = '0';
+        // var currentCount = 0;
+        // var biggest = 0;
+        // var biggestChar = '0';
+        // for (int i = 0; i < input.Length; i++)
+        // {
+        //     var currentChar = input[i];
+        //
+        //     if (currentChar == current)
+        //     {
+        //         currentCount++;
+        //     }
+        //     else
+        //     {
+        //         current = currentChar;
+        //         currentCount = 1;
+        //     }
+        //
+        //     if (currentCount > biggest)
+        //     {
+        //         biggest = currentCount;
+        //         biggestChar = currentChar;
+        //     }
+        // }
+        //
+        // return new Tuple<char?, int>(biggestChar == '0' ? null : biggestChar, biggest);
     }
 }

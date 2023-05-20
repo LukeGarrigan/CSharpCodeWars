fdf12e7b2a169c85e13c888bd285dd54bba86ccb
Luke <lukegarrigan8@gmail.com>
2023-05-20 16:23:07 +0100

    Soundex complete - disgusting code, no time to refactor

--- CSharpCodeWars/Kyu5/Soundex/Soundex.cs
+++ CSharpCodeWars/Kyu5/Soundex/Soundex.cs
@@ -5,7 +5,6 @@ namespace CSharpCodeWars.Kyu5.Soundex;
 
 public class Soundex
 {
-    private const string Consonants = "bcdfghjklmnpqrstvwxyz";
     public string CreateSoundex(string namesAsStr)
     {
         var names = namesAsStr.Split(" ");
@@ -13,16 +12,20 @@ public class Soundex
 
         var exclude = new char[]
         {
-            'h', 'w', 'a', 'e', 'i', 'o', 'u'
+            'a', 'e', 'i', 'o', 'u', 'y'
+        };
+
+        var initialExclude = new char[]
+        {
+            'h', 'w'
         };
 
         var output = new List<string>();
+        
         foreach (var name in names)
         {
-
             var firstLetter = name[0];
-
-            var newName = firstLetter + string.Concat(name.Substring(1).Where(c => !exclude.Contains(c)));
+            var newName = (firstLetter + string.Concat(name.Substring(1).Where(c => !initialExclude.Contains(c)))).ToLower();
             newName = Regex.Replace(newName, "[bfpv]", "1");
             newName = Regex.Replace(newName, "[cgjkqsxz]", "2");
             newName = Regex.Replace(newName, "[dt]", "3");
@@ -30,26 +33,38 @@ public class Soundex
             newName = Regex.Replace(newName, "[mn]", "5");
             newName = Regex.Replace(newName, "[r]", "6");
 
-
-            var strippedName = newName[0].ToString();
+            var trimmedName = name[0].ToString();
             for (var i = 1; i < newName.Length; i++)
             {
-                if (newName[i] != newName[i - 1])
+                if (!char.IsDigit(newName[i]) || !char.IsDigit(newName[i - 1]) || newName[i] != newName[i - 1])
                 {
-                    strippedName += newName[i];
+                    trimmedName += newName[i];
                 }
-
             }
 
-            if (strippedName.Length < 4)
+            trimmedName = trimmedName[0] + string.Concat(trimmedName.Substring(1).Where(c => !exclude.Contains(c)));
+
+            if (char.IsDigit(trimmedName[0]))
             {
-                strippedName += "000";
-                strippedName = strippedName.Substring(0, 4);
+                trimmedName = firstLetter + trimmedName.Substring(1);
             }
 
-            output.Add(strippedName);
+            if (trimmedName.Count(c => char.IsDigit(c)) < 3)
+            {
+                trimmedName += "000";
+            }
+
+            if (trimmedName.Length > 3)
+            {
+                trimmedName = trimmedName.Substring(0, 4);
+            }
+
+
+            output.Add(trimmedName);
         }
 
+
+        output = output.Select(w => w[0].ToString().ToUpper() + w.Substring(1)).ToList();
         return string.Join(" ", output);
     }
 }
--- CSharpCodeWars/Kyu5/Soundex/SoundexTests.cs
+++ CSharpCodeWars/Kyu5/Soundex/SoundexTests.cs
@@ -16,8 +16,14 @@ public class SoundexTests
       [Test]
       public void Arnie()
       {
-          Assert.AreEqual("S600 C560", _sut.CreateSoundex("Sarah Connor"));
-          // Assert.AreEqual("S600 C560", Dinglemouse.Soundex("Sarah Connor"));
+          // Assert.AreEqual("S600 C560", _sut.CreateSoundex("Sarah Connor"));
+          // Assert.AreEqual("R163", _sut.CreateSoundex("Robert"));
+          // Assert.AreEqual("R163", _sut.CreateSoundex("Rupert"));
+          // Assert.AreEqual("R150", _sut.CreateSoundex("Rubin"));
+          // Assert.AreEqual("T522", _sut.CreateSoundex("Tymczak"));
+          // Assert.AreEqual("P236", _sut.CreateSoundex("Pfister"));
+          // Assert.AreEqual("H555", _sut.CreateSoundex("Honeyman"));
+          Assert.AreEqual("E451", _sut.CreateSoundex("elmviina"));
           // Assert.AreEqual("S600 C560", Dinglemouse.Soundex("Sarah Connor"));
           // Assert.AreEqual("S600 C560", Dinglemouse.Soundex("Sarah Connor"));
           // Assert.AreEqual("S600 C560", Dinglemouse.Soundex("Sarah Connor"));

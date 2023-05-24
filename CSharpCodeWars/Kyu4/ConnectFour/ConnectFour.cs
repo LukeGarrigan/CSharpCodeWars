4e8535fb4be591c2af45098790b3c63848610f28
Luke <lukegarrigan8@gmail.com>
2023-05-24 07:48:33 +0100

    Fixed ConnectFour and refactored

--- CSharpCodeWars/Kyu4/ConnectFour/ConnectFour.cs
+++ CSharpCodeWars/Kyu4/ConnectFour/ConnectFour.cs
@@ -9,8 +9,6 @@ public class ConnectFour
     public string WhoIsWinner(List<string> pieces)
     {
         grid = new int[6, 7];
-        PrintArray();
-
         foreach (var piece in pieces)
         {
             var column = ToColumn(piece.Split("_")[0]);
@@ -19,10 +17,6 @@ public class ConnectFour
             var colourValue = colour == "Red" ? 1 : -1;
 
             var (x, y) = PlacePiece(column, colourValue);
-            
-
-            PrintArray();
-            
             if (IsWinner(x, y))
             {
                 return colour;
@@ -33,91 +27,24 @@ public class ConnectFour
         return "";
     }
 
-    private bool IsWinner(int row, int column)
+    private bool CheckNeighbours(int colour, int row, int column, int rowDelta, int columnDelta)
     {
-        var colour = grid[row, column];
-        // COLUMN NEIGHBOURS
-        int columnNeighbours = 0;
-
-        for (var i = column + 1; i < grid.GetLength(1); i++)
-        {
-            if (grid[row, i] == colour)
-            {
-                columnNeighbours++;
-            }
-            else
-            {
-                break;
-            }
-        }
-        
-        for (var i = column - 1; i >= 0; i--)
-        {
-            if (grid[row, i] == colour)
-            {
-                columnNeighbours++;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        if (columnNeighbours == 3)
-        {
-            return true;
-        }
-
-        // ROW NEIGHBOURS
-        var rowNeighbours = 0;
-        
-        for (var i = row + 1; i < grid.GetLength(0); i++)
-        {
-            if (grid[i, column] == colour)
-            {
-                rowNeighbours++;
-            }
-            else
-            {
-                break;
-            }
-        }
-        
-        for (var i = row - 1; i >= 0; i--)
-        {
-            if (grid[i, column] == colour)
-            {
-                rowNeighbours++;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        if (rowNeighbours == 3)
-        {
-            return true;
-        }
-        
-        
-        // DIAGONAL RIGHT
-        var diagonalRightNeighbours = 0;
+        var neighbours = 0;
 
         var currentColumn = column;
         var currentRow = row;
         while (true)
         {
-            currentColumn++;
-            currentRow--;
-            if (currentColumn >= grid.GetLength(1) || currentRow < 0)
+            currentColumn += columnDelta;
+            currentRow += rowDelta;
+            if (currentColumn < 0 || currentRow < 0 || currentRow >= grid.GetLength(0) || currentColumn >= grid.GetLength(0))
             {
                 break;
             }
 
             if (grid[currentRow, currentColumn] == colour)
             {
-                diagonalRightNeighbours++;
+                neighbours++;
             }
             else
             {
@@ -129,16 +56,16 @@ public class ConnectFour
         currentRow = row;
         while (true)
         {
-            currentColumn--;
-            currentRow++;
-            if (currentRow >= grid.GetLength(0) || currentColumn < 0)
+            currentColumn -= columnDelta;
+            currentRow -= rowDelta;
+            if (currentColumn < 0 || currentRow < 0 || currentRow >= grid.GetLength(0) || currentColumn >= grid.GetLength(0))
             {
                 break;
             }
 
             if (grid[currentRow, currentColumn] == colour)
             {
-                diagonalRightNeighbours++;
+                neighbours++;
             }
             else
             {
@@ -146,62 +73,21 @@ public class ConnectFour
             }
         }
   
-        if (diagonalRightNeighbours == 3)
-        {
-            return true;
-        }
-
-        // DIAGONAL LEFT
-        var diagonalLeftNeighbours = 0;
-
-        currentColumn = column;
-        currentRow = row;
-        while (true)
-        {
-            currentColumn--;
-            currentRow--;
-            if (currentColumn < 0 || currentRow < 0)
-            {
-                break;
-            }
-
-            if (grid[currentRow, currentColumn] == colour)
-            {
-                diagonalLeftNeighbours++;
-            }
-            else
-            {
-                break;
-            }
-        }
-        
-        currentColumn = column;
-        currentRow = row;
-        while (true)
-        {
-            currentColumn++;
-            currentRow++;
-            if (currentRow >= grid.GetLength(0) || currentColumn >= grid.GetLength(0) )
-            {
-                break;
-            }
-
-            if (grid[currentRow, currentColumn] == colour)
-            {
-                diagonalLeftNeighbours++;
-            }
-            else
-            {
-                break;
-            }
-        }
-  
-        if (diagonalLeftNeighbours == 3)
+        if (neighbours >= 3)
         {
             return true;
         }
 
         return false;
+    }
+
+    private bool IsWinner(int row, int column)
+    {
+        var colour = grid[row, column];
+        return CheckNeighbours(colour, row, column, 0, 1) ||
+               CheckNeighbours(colour, row, column, 1, 0) || 
+               CheckNeighbours(colour, row, column, 1, 1) || 
+               CheckNeighbours(colour, row, column, 1, -1);
     }
 
     private int ToColumn(string letter) => "ABCDEFG".IndexOf(letter, StringComparison.Ordinal);
@@ -220,20 +106,5 @@ public class ConnectFour
         }
 
         return (-1, -1);
-    }
-
-
-    public void PrintArray()
-    {
-        Console.WriteLine();
-        for (var i = 0; i < grid.GetLength(0); i++)
-        {
-            for (var j = 0; j < grid.GetLength(1); j++)
-            {
-                Console.Write(grid[i, j] + " ");
-            }
-
-            Console.WriteLine();
-        }
     }
 }
